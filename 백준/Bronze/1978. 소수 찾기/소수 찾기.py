0820d9c973a7974a2cdf856d652da5f1cd4e11df
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-06-24 05:41:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,20 @@
+count = int(input())
+data = input().split()
+
+total = 0
+divisor = []
+for d in data :
+    d = int(d)
+    if d == 1 : continue
+    elif d != 2 and d % 2 == 0 : continue
+    else : 
+        Prime = True
+        divisor = []
+        for i in range(2, d) :
+            if d % i == 0 : 
+                Prime = False  
+                break
+        if Prime is False : continue
+        total += 1
+
+print(total)

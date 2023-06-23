fb8fa37b1fbf2ad155b4343429fd629f4e0f344e
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-06-24 05:58:50 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 72 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,18 @@
+ArrSize = input().split()
+
+row = int(ArrSize[0])
+col = int(ArrSize[1])
+
+opA = []
+opB = []
+
+for _ in range(row):
+    opA.append(list(map(int, input().split())))
+   
+for _ in range(row):
+    opB.append(list(map(int, input().split())))
+    
+for r in range(row):
+    for c in range(col):
+        print(opA[r][c]+opB[r][c], end=" ")
+    print()

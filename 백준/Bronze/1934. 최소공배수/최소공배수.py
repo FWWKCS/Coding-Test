f25a387df9b63e6e8ce05d4856a6b0a43d8670f5
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-06-30 03:52:32 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,65 +1,28 @@
-def get_GCD (value) :
-    # 입력한 수의 최대공약수를 구하는 함수
-    tmp = { 
-        # 임시 딕셔너리    
-    }
-    if value == 1 : 
-        tmp[1] = 1
-        return tmp
-    
-    while value != 1 :
-        opr = 2
-        while True :
-            if value % opr == 0 : break
-            else : opr += 1
-        
-        if opr in tmp : tmp[opr] += 1
-        else : tmp[opr] = 1
+# 최소공배수(LCM) = 두 자연수의 곱 / 최대공약수(GCD)
 
-        value /= opr
-    
-    return tmp
+# 유클리드 호제법
+# 참조 : https://myjamong.tistory.com/138
 
-A = {
-    # 첫 수의 최대공약수 딕셔너리
-}
+# 두 수 A, B에 대하여 A > B일때, A % B = r0 과 
+# B, r0 에 대하여 B % r0 = r1, 
+# r0, r1 에 대하여 r1 % r0 = r2, .. 를 반복하여 rn이 0이 될때
+# r0는 최대 공약수(GCD)가 된다
 
-B = {
-    # 두번째 수의 최대공약수 딕셔너리
-}
+count = int(input())
 
-total = {
-    # A, B의 최소 공배수 딕셔너리
-}
+data = []
 
-data = [] # A, B 입력값
-
-testCase = int(input())
-
-for _ in range(testCase) :
+for _ in range(count) :
     data.append(list(map(int,input().split())))
 
-result = [] # print least common multiple
-
-# 데이터 처리 영역
 for d in data :
-    A, B = get_GCD(d[0]), get_GCD(d[1])
-    for a in A.keys() :
-        total[a] = A[a]
+    mul = d[0]*d[1]
+    A, B = d[0], d[1]
+    R = 1 
+    while R != 0 :
+        R = A % B
+        A, B = B, R
 
-    for b in B.keys() :
-        if b in total : total[b] = max(total[b], B[b])
-        else : total[b] = B[b]
-
-    # 최소공배수 계산
-    LCM = 1
-    for t in total.keys() :
-        LCM *= t**total[t]
-
-    result.append(LCM)
-
-    total.clear()
-
-for L in result : print(L)
-
-
+    GCD = A
+    LCM = mul // GCD
+    print(LCM)

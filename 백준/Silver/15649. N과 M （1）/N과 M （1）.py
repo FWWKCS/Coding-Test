aee22b815fc1cb0863aef6f22dc3a76514c6b7e5
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-07-23 06:00:42 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,21 @@
+def getPermutation(p_list) :
+    if len(p_list) == r : 
+        print(' '.join(str(e) for e in p_list))
+        return
+    
+    for k in range(1,n+1) :
+        if k not in p_list :
+            p_list.append(k)
+            getPermutation(p_list)
+            p_list.pop()
+
+
+# 순열 Permutation
+n, r = map(int, input().split()) # nPr
+
+p_list = []
+
+for k in range(1,n+1) :
+    p_list.append(k)
+    getPermutation(p_list)
+    p_list.pop()

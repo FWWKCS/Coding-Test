ce362f149d3a4130dba697ce6c1521a65d8aa728
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-07-22 23:33:41 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,16 +1,21 @@
-def get_factorial(x) :
-    total = 1
-    for k in range(2, x+1) :
-        total *= k
-
-    return total 
+import sys
+input = sys.stdin.readline
 
 testCase = int(input())
 
 for _ in range(testCase) :
-    K, N = map(int,input().split())
+    R, N = map(int, input().split()) # n choose r
 
-    # n choose k = n! / k!(n-k)!
-    nCr = get_factorial(N) // (get_factorial(K)*get_factorial(N-K))
+    memoization = [[0 for _ in range(R+1)] for _ in range(N+1)]
+    for h in range(N+1) :
+        memoization[h][0] = 1
+    
+    for n in range(1, N+1) :
+        for r in range(1, R+1) :
+            if n < r : break
 
-    print(nCr)
+            # 조합의 성질 
+            # n choose k = n-1 choose k + n-1 choose k-1
+            memoization[n][r] = memoization[n-1][r] + memoization[n-1][r-1]
+
+    print(memoization[N][R])

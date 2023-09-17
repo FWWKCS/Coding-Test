6cbd331f8975fb4b95f22f224ce222e66ad8acfb
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-09-17 23:32:48 +0900

    [Silver V] Title: 치즈버거 만들기 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/30020. 치즈버거 만들기 2/치즈버거 만들기 2.py
+++ 백준/Silver/30020. 치즈버거 만들기 2/치즈버거 만들기 2.py
@@ -1,40 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def makeBurger(log) :
-    for l in log :
-        print('ab'*l[1] + 'a')
-
-isFound = False
-
-def dfs(pa, ch) :
-    global log, isFound
-
-    if isFound or (pa > 0 and ch <= 0) or (pa > 2*ch) : return
-
-    if pa == ch + 1 :
-        print('YES')
-        log.append((pa,ch))
-        print(len(log))
-        makeBurger(log)
-        isFound = True
-        return 
-
-    for k in range(ch+1, 1, -1) :
-        if isFound : break
-        log.append((k, k-1))
-        dfs(pa-k, ch-(k-1))
-        log.pop()
-
-
 N, M = map(int, input().split())
 
-# 치즈가 패티보다 많으면 절대 불가능
-# 한 버거에 패티와 치즈를 최소한으로 사용할 때 패티가 치즈보다 지나치게 많으면 불가능
+count = 0
+burger = [] # (pa, ch)
 
-log = []
+while N != M+1 :
+    if N <= M or N > M*2 :
+        print('NO')
+        sys.exit()
+    
+    burger.append((2, 1))
+    N -= 2
+    M -= 1
 
-if N > M and N <= 2*M: 
-    dfs(N, M)
+burger.append((N, M))
+print('YES')
+print(len(burger))
 
-if not isFound : print('NO')
+for b in burger :
+    print('ab' * b[1] + 'a')

a38e34971bd1d6bf2f278a063727237ae28707af
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-09-21 16:24:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,17 +1,15 @@
 import collections
 
 N, K = map(int, input().split())
-queue = collections.deque([i for i in range(1,N+1)])
+queue = collections.deque([i for i in range(1, N+1)])
 
+# 순환
 permutation = []
-
 while queue :
     queue.rotate(-(K-1))
     permutation.append(queue.popleft())
 
+# 형식 출력
 print('<', end = '')
-for p in range(N) :
-    if p != N-1 :
-        print(permutation[p], end = ', ')
-    else :
-        print(permutation[p], end = '>')
+for p in permutation[:N-1] : print(p, end = ', ')
+print(permutation[-1], end = '>')

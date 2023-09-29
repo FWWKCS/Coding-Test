990ec7da3a16e19fbf520ddb230e5145411a49f0
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-09-29 18:46:42 +0900

    [Silver III] Title: Four Squares, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,17 +1,27 @@
-dp = [5 for i in range(50001)]
-# dp[i] : i를 만드는데 필요한 최소 개수의 제곱수
-dp[0] = 0
+import math
+import sys
+input = sys.stdin.readline
 
-# k^2수는 무조건 1
-for k in range(1, int(50000 ** 0.5) + 1) :
-    dp[k**2] = 1
+def is_square(n) :
+    k = int(math.sqrt(n))
+    return k*k == n
 
-for k in range(50001) :
-    if dp[k] == 1 : continue
+def get_result(n) :
+    # n이 완전 제곱수인 경우 (1)
+    if is_square(n) : return 1
 
-    num = int(k ** 0.5)
-    for i in range(num, (num//2), -1) :
-        dp[k] = min(dp[k], dp[i**2] + dp[k-(i**2)])
+    # n이 두 완전 제곱수로 이루어지는 경우 (2)
+    x = int(math.sqrt(n))
+    for i in range(x, (x//2), -1) :
+        if is_square(n - (i*i)) : return 2
 
+    # n이 세 완전 제곱수로 이루어지는 경우 (3)
+    for i in range(x, (x//2), -1) :
+        y = int(math.sqrt(n-(i*i)))
+        for j in range(y, (y//2), -1) :
+            if is_square(n - (i*i) - (j*j)) : return 3
+
+    # 그 외의 경우는 최대 제곱 항의 수 4
+    return 4
 n = int(input())
-print(dp[n])
+print(get_result(n))

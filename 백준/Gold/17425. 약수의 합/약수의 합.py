d57d6704ab6134350fe9dfab25454afc341ed573
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-09-29 16:34:19 +0900

    [Gold IV] Title: 약수의 합, Time: 340 ms, Memory: 131584 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.py
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -1,20 +1,19 @@
+import math
 import sys
 input = sys.stdin.readline
 
 T = int(input())
 
-# g_x 리스트 생성
-g_x = [0] + ([1] * 1000000)
+f_x = [0] + ([1] * 1000000)
 
 for k in range(2, 1000001) :
     for i in range(k, 1000001, k) :
-        g_x[i] += k
+        f_x[i] += k
 
-# 동적 계획법
-for g in range(2,len(g_x)) :
-    g_x[g] = g_x[g] + g_x[g-1]
+for g in range(2,len(f_x)) :
+    f_x[g] = f_x[g] + f_x[g-1]
 
 for _ in range(T) :
     N = int(input())
 
-    print(g_x[N])
+    print(f_x[N])   

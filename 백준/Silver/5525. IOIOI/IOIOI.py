899d703b65a2110d9974d07a18fdc1b34992498c
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-10-02 18:54:48 +0900

    [Silver I] Title: IOIOI, Time: 296 ms, Memory: 33212 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,11 +6,28 @@ length = int(input())
 S = input().rstrip()
 
 count = 0
-for l in range(2*N + 1, length+1) :
-    start = l-(2*N+1)
-    if S[start] == 'O' : continue
-    
-    find = 'IO' * N + 'I'
-    if S[start:l] == find : count += 1
+length = 0 # 만들어진 문자열 길이를 실시간 체크
+flag = 1 
+# 1이면 I를 필요로함, 0이면 O를 필요로함
+
+for s in S :
+    # 플래그 체크
+    if (flag == 1 and s == 'I') :
+        length += 1
+        flag = 0
+    elif flag == 0 and s == 'O' :
+        length += 1
+        flag = 1
+    else : # 플래그와 일치하지 않는 경우 초기화
+        if s == 'I' :
+            flag = 0
+            length = 1
+        elif s == 'O' :
+            flag = 1
+            length = 0
+        
+    if length == 2*N + 1 : 
+        count += 1
+        length -= 2
 
 print(count)

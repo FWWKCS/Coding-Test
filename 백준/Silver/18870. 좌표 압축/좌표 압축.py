68a37571e95283a02999500b21fe941b217d5a42
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-10-11 01:05:59 +0900

    [Silver II] Title: 좌표 압축, Time: 2008 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,29 +1,14 @@
-def binary_search (findValue, data) :
-    start = 0
-    end = len(data) - 1 
-    while start <= end :
-        pivot = (start + end) // 2
-        if findValue == data[pivot] : return pivot
-        elif findValue > data[pivot] : start = pivot + 1
-        else : end = pivot - 1
+N = int(input())
+pos = list(map(int, input().split()))
 
-    return -1
+pos_dict = {
+    # 좌표 : 순서
+}
+sorted_pos = sorted(list(set(pos)))
 
-import sys
-input = sys.stdin.readline
+# 딕셔너리 값 추가
+for k in range(len(sorted_pos)) :
+    pos_dict[sorted_pos[k]] = k
 
-count = int(input())
-
-data = list(map(int,input().split()))
-
-dataSet = list(set(data))
-# sorted()는 병합정렬을 기반으로 만들어지므로 최악에서도 O(NlogN) 의 시간을 가짐
-dataSet = sorted(dataSet)
-
-# 이진 탐색을 이용해 O(log_2 N)의 시간을 가짐
-# dataSet.index(k)는 O(N)의 시간으로 시간이 더 걸림
-
-start = 0
-end = len(dataSet)
-for i in data :
-    print(binary_search(i, dataSet), end = ' ')
+for p in pos :
+    print(pos_dict[p], end = ' ')

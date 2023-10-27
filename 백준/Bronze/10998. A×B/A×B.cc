e55521e04109856ab3e2448c8ac843e24d7404a7
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-10-27 17:22:14 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[2];
+    cin >> arr[0];
+    cin >> arr[1];
+    int result = arr[0] * arr[1];
+    cout << result << endl;
+}

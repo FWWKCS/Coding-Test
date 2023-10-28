d313313cd791183f47aa9ba959d2f21627e35f4b
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-10-29 02:20:55 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int T; cin >> T;
-    int AB[T][2];
-    for(int i=0;i<T;i++){
-        cin >> AB[i][0];
-        cin >> AB[i][1];
-    }
-    for (int i=0;i<T;i++){
-        cout << "Case #" << i+1 << ": " << AB[i][0]+AB[i][1] << endl;
+int main() {
+    int N;
+    cin >> N;
+
+    int a,b;
+    for (int i= 0; i < N; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a+b << endl;
     }
 }

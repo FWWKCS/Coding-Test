1937f6f4f15c95d81cd6b7ec5424615732ce887d
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-10-29 22:08:39 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 180 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
     int testcase, a, b;
-    scanf("%d", &testcase);
+    cin >> testcase;
 
     for (int i = 0; i < testcase; i++) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a+b);
+        cin >> a >> b;
+        cout << a+b << '\n';
     }
 }

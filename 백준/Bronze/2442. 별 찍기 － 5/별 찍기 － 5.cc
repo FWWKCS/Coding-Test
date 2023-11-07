f26a7d6158fa6dce29293bc551b04aecb280b474
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-11-07 21:47:42 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        // 공백 출력
+        for (int b = 0; b < N-i; b++) cout << " ";
+        // 별 출력
+        for (int s = 0; s < (2*i)-1; s++) cout << "*";
+        // 개행
+        cout << '\n';
+    }
+}

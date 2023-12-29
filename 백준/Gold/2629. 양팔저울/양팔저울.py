c5166151269ba7685bb00c575485ddb708c2ab39
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2023-12-29 14:09:14 +0900

    [Gold III] Title: 양팔저울, Time: 792 ms, Memory: 41436 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -14,23 +14,20 @@ for r in range(N):
             continue
 
         # r번째 추 무게와 같은 c는 판별가능하다 (r번 추 하나를 사용)
-        if pendulum[r] == c : possibility[r][c] = True
+        elif pendulum[r] == c : possibility[r][c] = True
 
         # r-1번 추까지 사용한 기록중 r번 추에서 찾을 무게 c를 뺀 곳의 무게는 판별 가능하다
-        if r > 0 and possibility[r-1][abs(pendulum[r]-c)]:
+        elif r > 0 and possibility[r-1][abs(pendulum[r]-c)]:
             possibility[r][c] = True
 
         # r-1번 추까지 사용한 기록중 r번 추에서 찾을 무게 c와 더한 무게는 판별 가능하다
-        if r > 0 and pendulum[r]+c < 40001 and possibility[r-1][pendulum[r]+c]:
+        elif r > 0 and pendulum[r]+c < 40001 and possibility[r-1][pendulum[r]+c]:
             possibility[r][c] = True
         
         # r-1번 추까지 사용한 기록이 판별 가능하면 r번 추까지의 경우에도 판별 가능하다 
         # (r번 추를 사용하지 않으면 된다)
-        if r > 0 and possibility[r-1][c]:
+        elif r > 0 and possibility[r-1][c]:
             possibility[r][c] = True
-
-# for p in possibility: print(p)
-
 
 for m in marble:
     if possibility[-1][m]: print('Y', end = ' ')

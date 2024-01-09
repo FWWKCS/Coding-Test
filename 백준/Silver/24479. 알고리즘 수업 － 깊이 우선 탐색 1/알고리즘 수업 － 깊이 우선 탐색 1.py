aa3b0ef8a5ce19ee40b6f68d60c2f8e63fd383af
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-01-09 12:58:01 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 2244 ms, Memory: 1014876 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,34 +1,31 @@
 import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 5)
 
-def dfs(V, E, R) : # V : 정점 집합, E : 간선 집합, R : 시작 정점
-    global order
-    vertex[R] = order
-    visited[R] = True
-    for x in sorted(list(E[R])) :
-        if not visited[x[0]] :
-            order += 1
-            dfs(V, E, x[0])
+def dfs(graph, start, visited, temp):
+    global count
+    visited.add(start)
+    temp[start] = count
+    count += 1
+    nbr = sorted(list(graph[start] - visited))
+    for v in nbr:
+        if v not in visited:
+            dfs(graph, v, visited, temp)
+                
+n, m, r = map(int, input().split())
+graph = {}
+global count
+count = 1
+temp = [0] * (n+1)
 
-N, M, R = map(int, input().split())
+for i in range(1, n+1):
+    graph[i] = set()
+    
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].add(b)
+    graph[b].add(a)
+    
+dfs(graph, r, set(), temp)
 
-# 정점 집합
-order = 1
-vertex = [0 for _ in range(N+1)]
-# 정점 집합
-visited = [False for _ in range(N+1)]
-# 간선 집합
-enter = {
-    # E(V) = {(x, e), (...), ... } # x : 종점, e : 가중치
-}
-for k in range(1, N+1) : enter[k] = set()
-
-for _ in range(M) :
-    u, v = map(int, input().split())
-    enter[u].add((v, 1))
-    enter[v].add((u, 1)) # 무방향 그래프, 양방향 대칭
-
-dfs(vertex, enter, R)
-
-for v in vertex[1:] : print(v)
+for i in range(1, n+1):
+    print(temp[i])

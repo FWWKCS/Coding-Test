01f41d4a3b4f3916905fc51313d99ab8bc35e136
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-01-09 13:01:18 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 732 ms, Memory: 66520 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(R):
+    global graph, visit, count
+    visited = {R}
+    queue = deque([(R, count)])
+
+    while queue:
+        R, C = queue.popleft()
+        visit[R] = C
+        if R not in graph: continue
+
+        for target in graph[R]:
+            if target not in visited:
+                count += 1
+                visited.add(target)
+                queue.append((target, count))
+
+N, M, R = map(int, input().split())
+graph = {
+    # 시작 정점: [인접 정점]
+}
+visit = [0 for _ in range(N+1)]
+count = 1
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    if u not in graph:
+        graph[u] = [v]
+    else:
+        graph[u].append(v)
+    
+    if v not in graph:
+        graph[v] = [u]
+    else:
+        graph[v].append(u)
+
+for x in graph.keys():
+    graph[x].sort(key=lambda x : -x) # 오름차순 정렬
+
+bfs(R)
+for i in range(1, N+1):
+    print(visit[i])

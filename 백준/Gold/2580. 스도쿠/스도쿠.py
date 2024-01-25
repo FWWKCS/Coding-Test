5d35a0acc8ba17e70830040a7d2245f10b0ea1c4
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-01-25 23:34:56 +0900

    [Gold IV] Title: 스도쿠, Time: 1696 ms, Memory: 141536 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,56 +1,90 @@
+import collections
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(10000)
 
-def findValidValue(r, c, selected) :
+def elimination(r, c, check) :
     global board
-    # 가로 탐색
+    # 가로 소거
     for i in range(9) :
-        if board[r][i] == selected : return False
+        if board[r][i] == 0 : continue
 
-    # 세로 탐색
+        check[board[r][i]-1] = True
+
+    # 세로 소거
     for i in range(9) :
-        if board[i][c] == selected : return False
+        if board[i][c] == 0 : continue
         # 위에서 이미 제거된 수는 스킵
+        if check[board[i][c]-1] : continue
+
+        check[board[i][c]-1] = True
 
     # 대각선 소거 ( 구간 나눠 반복문 구분 )
-    x = (c // 3) * 3 
-    y = (r // 3) * 3    
+    x, y = -1, -1 # 각 블럭의 시작좌표
+    if r <= 2 and c <= 2 : 
+        x, y = 0, 0
+    elif r <= 2 and c >= 3 and c <= 5 : 
+        x, y = 3, 0
+    elif r <= 2 and c >= 6 : 
+        x, y = 6, 0
+    elif r >= 3 and r <= 5 and c <= 2 : 
+        x, y = 0, 3
+    elif r >= 3 and r <= 5 and c >= 3 and c <= 5 : 
+        x, y = 3, 3
+    elif r >= 3 and r <= 5 and c >= 6 :
+        x, y = 6, 3
+    elif r >= 6 and c <= 2 :
+        x, y = 0, 6
+    elif r >= 6 and c >= 3 and c <= 5 :
+        x, y = 3, 6
+    elif r >= 6 and c >= 6 :
+        x, y = 6, 6
 
     for i in range(y,y+3) :
         for j in range(x, x+3) :
-            if board[i][j] == selected : return False
+            if board[i][j] == 0 : continue
 
-    # 모든 조건을 충족
-    return True
+            if check[board[i][j]-1] : continue
+
+            check[board[i][j]-1] = True
+        
+    # 완성된 check 리턴
+    return check
 
 def sudoku(depth) :
     global put, board
 
     # 조건을 모두 만족하고 다 채웠으면 결과를 출력하고 종료
     if depth == len(put) :
-        for b in board :
-            print(*b)
+        for r in range(9) :
+            for c in range(9) :
+                print(board[r][c], end = ' ')
+            print()
         sys.exit()
 
     # 채워야할 인덱스 결정
     current_R = put[depth][0]
     current_C = put[depth][1]
- 
-    # 1부터 9까지 조건을 만족하는 숫자 탐색
+
+    # 1부터 9중 가능한 숫자만 남긴 후 재귀 호출 ( 소거법 )
+    check = [False for _ in range(9)] 
+    check = elimination(current_R, current_C, check)
+
     for k in range(1,10) :
-        if findValidValue(current_R, current_C, k) : 
-            # 대입 가능한 수를 board에 추가
-            board[current_R][current_C] = k
-            sudoku(depth+1)
-            # 복구
-            board[current_R][current_C] = 0
+        if check[k-1] : continue
 
+        # 대입 가능한 수를 board에 추가
+        board[current_R][current_C] = k
+        sudoku(depth+1)
+        # 복구
+        board[current_R][current_C] = 0
 
-board = [list(map(int, input().split())) for _ in range(9)]
+board = []
+for _ in range(9) :
+    line = list(map(int, input().split()))
+    board.append(line)
 
 # 채워야 할 곳 탐색
-put = []
+put = collections.deque([])
 for r in range(9) :
     for c in range(9) :
         if board[r][c] == 0 :

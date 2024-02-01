d243bc2537fa5290725040b57a0ca1b9169e0323
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-01 19:41:46 +0900

    [Gold IV] Title: 트리, Time: 212 ms, Memory: 37204 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,46 +1,38 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+
+def dfs(prev, current):
+    visited.add(current)
+    for v in graph[current]:
+        if v == prev: continue
+        # 이동할 수 있는 정점이 이미 방문되어있으면 사이클
+        if v in visited: return False
+        if not dfs(current, v): return False
+    
+    return True
 
 case_count = 1
 while True:
     n, m = map(int, input().split())
     if n == 0 and m == 0: break
 
-    graph = { 
-        # 시작 정점 : [ 도착 정점 ]
+    graph = {
+        # 시작 정점 : { 출발 정점 }
     }
-    for i in range(1,n+1): graph[i] = []
-    for _ in range(m):
+    for i in range(1, n+1): graph[i] = []
+    for _ in range(m): 
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
-
-    processed = set()
+    
+    visited = set()
     T = 0
-    for x in range(1, n+1):
-        if x in processed: continue
+    for k in range(1, n+1):
+        if k in visited: continue
 
-        visited = set()
-        E = 0 # 형태를 이루는 간선 개수
-        queue = deque([x])
-        while queue:
-            current = queue.popleft()
-            visited.add(current)
-            processed.add(current)
-            E += len(graph[current])
+        if dfs(-1, k): T += 1
 
-            for k in graph[current]:
-                if k not in visited:
-                    queue.append(k)
-
-        if len(visited) - (E//2) == 1: T += 1
-
-    if T == 0:
-        print(f'Case {case_count}: No trees.')
-    elif T == 1:
-        print(f'Case {case_count}: There is one tree.')
-    else:
-        print(f'Case {case_count}: A forest of {T} trees.')
-
-    case_count += 1      
+    if T == 0: print(f'Case {case_count}: No trees.')
+    elif T == 1: print(f'Case {case_count}: There is one tree.')
+    else: print(f'Case {case_count}: A forest of {T} trees.')
+    case_count += 1

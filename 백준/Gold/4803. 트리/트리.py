d4ba0c73a5cefd0d6424467c0713b6348fa78baf
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-02 14:23:49 +0900

    [Gold IV] Title: 트리, Time: 228 ms, Memory: 123352 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,36 +1,47 @@
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-def dfs(prev, current):
-    visited.add(current)
-    for v in graph[current]:
-        if v == prev: continue
-        # 이동할 수 있는 정점이 이미 방문되어있으면 사이클
-        if v in visited: return False
-        if not dfs(current, v): return False
-    
-    return True
+def find_parent(target):
+    if target == pointer[target]: return target
+
+    pointer[target] = find_parent(pointer[target]) # 경로 압축
+    return pointer[target]
+
+def union(a, b):
+    x = find_parent(a)
+    y = find_parent(b)
+
+    if x < y: pointer[y] = x
+    else: pointer[x] = y
+
+def is_union(a, b):
+    x = find_parent(a)
+    y = find_parent(b)
+
+    if x == y: return True
+    else: return False    
 
 case_count = 1
 while True:
     n, m = map(int, input().split())
     if n == 0 and m == 0: break
 
-    graph = {
-        # 시작 정점 : { 출발 정점 }
-    }
-    for i in range(1, n+1): graph[i] = []
+    cycled = set()
+    pointer = [i for i in range(0, n+1)]
+    T = 0
     for _ in range(m): 
         a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-    
-    visited = set()
-    T = 0
-    for k in range(1, n+1):
-        if k in visited: continue
+        if is_union(a, b):
+            cycled |= {a, b}
+            continue
 
-        if dfs(-1, k): T += 1
+        union(a, b)
+
+    cycled_parent = set()
+    for i in range(1, n+1): find_parent(i)
+    for i in cycled: cycled_parent.add(find_parent(i)) 
+    T = len(set(pointer)-{0}) - len(cycled_parent)
 
     if T == 0: print(f'Case {case_count}: No trees.')
     elif T == 1: print(f'Case {case_count}: There is one tree.')

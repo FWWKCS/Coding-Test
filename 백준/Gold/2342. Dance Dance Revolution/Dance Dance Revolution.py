a06290f0beba3cc494701ae729727d1a5abb728e
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-13 11:24:40 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 848 ms, Memory: 222076 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -19,11 +19,13 @@ dp[1][step[1]][0] = 2
 for k in range(2, len(step)-1):
     for r in range(5):
         for c in range(5):
-            if r != c:
-                # 왼발이 k번 스텝으로 이동
-                dp[k][step[k]][c] = min(dp[k][step[k]][c], dp[k-1][r][c] + check_power(step[k], r))
-                # 오른발이 k번 스텝으로 이동
-                dp[k][r][step[k]] = min(dp[k][r][step[k]], dp[k-1][r][c] + check_power(step[k], c))
+            if r != c and dp[k-1][r][c] != float('inf'):
+                if step[k] != c:
+                    # 왼발이 k번 스텝으로 이동
+                    dp[k][step[k]][c] = min(dp[k][step[k]][c], dp[k-1][r][c] + check_power(step[k], r))
+                if step[k] != r:
+                    # 오른발이 k번 스텝으로 이동
+                    dp[k][r][step[k]] = min(dp[k][r][step[k]], dp[k-1][r][c] + check_power(step[k], c))
 
 answer = float('inf')
 for r in range(5):

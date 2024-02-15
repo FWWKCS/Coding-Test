132684f5eed7c9d4191de476a4c1513615c6d101
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-15 20:26:38 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze I/2693. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Bronze I/2693. N번째 큰 수/N번째 큰 수.cc
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     vector<int> num(10);
     int T; cin >> T;
     for (int i = 0; i < T; i++) {

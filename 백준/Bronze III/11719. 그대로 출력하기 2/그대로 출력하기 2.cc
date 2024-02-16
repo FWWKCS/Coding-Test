926e2de93446383590ec7f67d5af96f7e5a41797
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-16 13:23:58 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze III/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    string input;
+    while (getline(cin, input)) {
+        if (input == "EOF") break;
+        cout << input << '\n';
+    }
+}

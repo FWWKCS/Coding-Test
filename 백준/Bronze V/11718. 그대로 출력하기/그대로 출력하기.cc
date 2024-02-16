aefcd5828adb9e7cf9827d5f06ee058672018412
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-02-16 13:23:15 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze V/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze V/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
     string input;
     while (getline(cin, input)) {
         if (input == "EOF") break;

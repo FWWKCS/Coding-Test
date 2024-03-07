ff7aeba745b4fb0fa20fba2c7e49c7e643bfde2d
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-03-07 14:14:25 +0900

    [Bronze III] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int count = 0;
+    string input;
+    while(getline(cin, input)) count++;
+    cout << count;
+}

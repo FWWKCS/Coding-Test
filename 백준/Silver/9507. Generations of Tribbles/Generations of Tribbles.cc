579f9c90cb85df570072bc65870ca56db8459fe9
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-03-08 00:17:24 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.cc
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.cc
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int koong(vector<unsigned long long>& F, int n) {
+long long koong(vector<long long>& F, int n) {
     if (F[n] != 0) return F[n];
 
     if (n < 2) F[n] = 1;
     else if (n == 2) F[n] = 2;
     else if (n == 3) F[n] = 4;
-    else F[n] = F[n-1] + F[n-2] + F[n-3] + F[n-4];
+    else F[n] = koong(F, n-1) + koong(F, n-2) + koong(F, n-3) + koong(F, n-4);
 
     return F[n];
 }
@@ -19,11 +19,10 @@ int main() {
 
     int t; cin >> t;
     int n;
-    vector<unsigned long long> F(68, 0);
-    for (int i = 0; i <= 67; i++) koong(F, i);
+    vector<long long> F(68, 0);
 
     for (int i = 0; i < t; i++) {
         cin >> n;
-        cout << F[n] << '\n';
+        cout << koong(F, n) << '\n';
     }
 }

8bf75be69dda971310623b44255b20da4c497f8f
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-03-28 09:25:44 +0900

    [Silver III] Title: 다음 순열, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+
+    vector<int> target;
+    int x;
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+        target.push_back(x);
+    }
+
+    bool found = next_permutation(target.begin(), target.end());
+    if (found) for (auto x : target) cout << x << ' ';
+    else cout << -1;
+}

7b2b6a9abebdfac4a97174652d5c329e4e02ded6
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-05-02 09:19:41 +0900

    [Silver V] Title: 복붙의 달인, Time: 12 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/11008. 복붙의 달인/복붙의 달인.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T; cin >> T;
+    string s, p;
+    for (int i = 0; i < T; i++) {
+        cin >> s >> p;
+        int count = 0;
+        string stack = "";
+        for (int j = 0; j < s.length(); j++) {
+            stack.push_back(s[j]);
+            if (stack.size() >= p.length() && stack.substr(stack.size()-p.length(), p.length()) == p) {
+                count++;
+                for (int x = 0; x < p.length(); x++) stack.pop_back();
+            }
+        }
+
+        count += stack.size();
+        cout << count << '\n';
+    }
+}

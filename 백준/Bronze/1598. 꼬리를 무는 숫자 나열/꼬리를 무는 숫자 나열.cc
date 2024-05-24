589f6cb15d584a5acb193b87f1c415bde00ed55d
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-05-24 09:15:29 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B; cin >> A >> B;
+    int rA, rB, cA, cB;
+
+    if (A % 4 != 0) rA = A / 4 + 1;
+    else rA = A / 4;
+
+    if (B % 4 != 0) rB = B / 4 + 1;
+    else rB = B / 4;
+
+
+    cA = A % 4;
+    if (cA == 0) cA = 4;
+
+    cB = B % 4;
+    if (cB == 0) cB = 4;
+
+    // printf("A = %d %d, B = %d %d\n", rA, cA, rB, cB);
+    cout << abs(rA - rB) + abs(cA - cB);
+}

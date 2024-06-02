5d2f6266bb385ceb68e7364c56d840d9fc914eb1
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-06-03 08:00:51 +0900

    [Gold V] Title: 알약, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long Cn(int n, long long C) {
+    return 2 * (2 * n + 1) * C / (n + 2);
+}
+
+int main() {
+    // Catalan number
+
+    vector<long long> catalan(1, 1);
+    for (int i = 1; i < 30; i++) {
+        catalan.push_back(Cn(i, catalan.back()));
+    }
+
+    int Q;
+    while (true) {
+        cin >> Q;
+        if (Q == 0) break;
+
+        cout << catalan[Q-1] << '\n';
+    }
+}

43ecf445ee28a36a32672af8a9b182cd91f978a9
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-06-05 16:02:53 +0900

    [Bronze I] Title: 814 - 1, Time: 0 ms, Memory: 2020 KB, Score: 17 point -BaekjoonHub

--- 백준/Bronze/16972. 814 － 1/814 － 1.cc
+++ 백준/Bronze/16972. 814 － 1/814 － 1.cc
@@ -8,14 +8,19 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
+    // 시드값을 얻기 위해 random_device를 사용
+    std::random_device rd;
+
+    // random_device를 이용해 시드된 Mersenne Twister 엔진을 생성
+    std::mt19937 gen(rd());
+
+    // 0에서 99까지 균등 분포 정의
+    std::uniform_int_distribution<> dis(-8139, 8139);
+
     int count = 0;
-    for (int y = -8140; y <= 8140; y+=20) {
-        for (int x = -8140; x <= 8140; x+=20) {
-            cout << x << ' ' << y << '\n';
-            count++;
-            if (count == 812) break;
-        }
-        if (count == 812) break;
+    while (count < 812) {
+        cout << dis(gen) << ' ' << dis(gen) << '\n';
+        count++;
     }
-    cout << "8140 8140\n8139 8139";
+    cout << "-8140 -8140\n8140 8140";
 }

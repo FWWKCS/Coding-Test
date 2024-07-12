00a044502f03a11e1cc5e1e90a3e28f97f3034ad
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-07-12 16:08:31 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define INF 1'000'000
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    int C, N; cin >> C >> N;
+    vector<pair<int, int>> data = { make_pair(INF, INF) };
+    vector<vector<int>> dp(N+1, vector<int>(C+1, INF));
+
+    for (int i = 0; i < N; i++) {
+        int p, v; cin >> p >> v;
+        data.push_back(make_pair(p, v));
+    }
+
+    for (int r = 1; r <= N; r++) {
+        for (int k = 1; k <= C; k++) {
+            int own, p = data[r].first, v = data[r].second;
+            if (k % v != 0) own = ((k / v) + 1) * p;
+            else own = (k / v) * p;
+
+            dp[r][k] = min(dp[r-1][k], own);
+            if (k - v >= 0) dp[r][k] = min(min(dp[r][k], dp[r-1][k-v] + p), dp[r][k-v] + p);
+        }
+    }
+
+    // for (auto r : dp) {
+    //     for (int c : r) {
+    //         if (c >= INF) cout << "INF ";
+    //         else cout << c << ' ';
+
+    //     }
+    //     cout << '\n';
+    // }
+
+    cout << dp[N][C];
+}

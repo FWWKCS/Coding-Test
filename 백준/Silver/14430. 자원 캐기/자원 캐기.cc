b4e34460641c1185158e1d20f9e936e88748f13e
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-07-26 14:20:09 +0900

    [Silver II] Title: 자원 캐기, Time: 4 ms, Memory: 2376 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.cc
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.cc
@@ -11,10 +11,7 @@ int main() {
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
             int x; cin >> x;
-            if (x == 1) {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1;
-            }
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + x;
         }
     }
 

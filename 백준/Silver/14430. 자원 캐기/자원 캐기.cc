67359fbfe6ff5c0649c991fbae16ee6e923c4e67
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-07-26 14:18:40 +0900

    [Silver II] Title: 자원 캐기, Time: 4 ms, Memory: 2376 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int dp[301][301] = {0, };
+
+int main() {
+    FASTIO;
+
+    int N, M; cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            int x; cin >> x;
+            if (x == 1) {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1;
+            }
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << dp[N][M];
+}

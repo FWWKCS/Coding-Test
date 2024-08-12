6b59bcba127caf887eb14c5da17896b09d645411
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-08-12 16:02:05 +0900

    [Silver II] Title: 수익, Time: 336 ms, Memory: 4828 KB -BaekjoonHub

---
+++ 백준/Silver/4097. 수익/수익.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    int N;
+    while (cin >> N) {
+        if (N == 0) break;
+        
+        int arr[N] = {0, };
+        for (int i = 0; i < N; i++) {
+            int x; cin >> x;
+            arr[i] = x;
+        }
+
+        long long dp[N] = {0, };
+        dp[0] = arr[0];
+
+        long long maxV = -10'000'000'000;
+        for (int i = 1; i < N; i++) {
+            dp[i] = max(dp[i-1]+arr[i], (long long)arr[i]);
+            maxV = max(maxV, dp[i]);
+        }
+
+        cout << maxV << '\n';
+    }
+}

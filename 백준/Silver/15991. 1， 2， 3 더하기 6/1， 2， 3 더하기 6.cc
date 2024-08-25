b067b82dbf1814b12349374e8397200ea7f322eb
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-08-25 21:46:01 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.cc
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.cc
@@ -8,25 +8,21 @@ using namespace std;
 int main() {
     FASTIO;
     
-    vector<vector<long long>> dp(4, vector<long long>(MAX+1, 0));
-    dp[0][1] = 1, dp[1][0] = 1, dp[1][1] = 1;
-    dp[2][0] = 1, dp[2][2] = 1;
-    dp[3][0] = 1, dp[3][3] = 1;
+    vector<long long> dp(MAX+1, 0);
+    dp[0] = 1, dp[1] = 1; dp[2] = 1; dp[3] = 1;
 
-    for (int k = 2; k <= MAX; k++) {
-        for (int r = 1; r <= 3; r++) {
-            int a = k - 2*r;
-            if (a < 0) continue;
-            else if (a == 0) dp[r][k] = dp[r][0];
-            else dp[r][k] = (dp[1][a] + dp[2][a] + dp[3][a]) % MOD;
-        }
+    for (int i = 2; i <= MAX; i++) {
+        if (i >= 2) dp[i] += dp[i-2];
+        if (i >= 4) dp[i] += dp[i-4];
+        if (i >= 6) dp[i] += dp[i-6];
 
-        dp[0][k] = (dp[1][k] + dp[2][k] + dp[3][k]) % MOD;
+        dp[i] %= MOD;
     }
-    
+
+
     int T; cin >> T;
     while (T--) {
         int n; cin >> n;
-        cout << dp[0][n] << '\n';
+        cout << dp[n] << '\n';
     }
 }

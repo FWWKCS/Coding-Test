e2c16a6fbef7cde11d07c7e5d98191c0730704d9
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-08-31 20:37:54 +0900

    [Gold V] Title: 개업, Time: 300 ms, Memory: 2316 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.cc
+++ 백준/Gold/13910. 개업/개업.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define INF 1'000'000'009
 using namespace std;
 
 int main() {
+    FASTIO;
+
     set<int> subN;
     int N, M; cin >> N >> M;
     vector<int> woks;

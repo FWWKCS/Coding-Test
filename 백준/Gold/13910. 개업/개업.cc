14e18addf9d2b677d4f98f7d8553f065509e73fa
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-08-31 20:50:28 +0900

    [Gold V] Title: 개업, Time: 28 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.cc
+++ 백준/Gold/13910. 개업/개업.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
 #define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define INF 1'000'000'009
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     FASTIO;
 
-    set<int> subN;
+    vector<int> subN;
     int N, M; cin >> N >> M;
     vector<int> woks;
     vector<int> dp(N+1, INF);
@@ -16,17 +16,19 @@ int main() {
     for (int i = 0; i < M; i++) {
         int x; cin >> x;
         woks.push_back(x);
+        subN.push_back(x);
     }
 
     // 웍들 중 2개를 사용해 가능한 그릇 체크
     for (int i = 0; i < M-1; i++) {
         for (int j = i+1; j < M; j++) {
-            subN.insert(woks[i]);
-            subN.insert(woks[j]);
-            subN.insert(woks[i] + woks[j]);
+            subN.push_back(woks[i] + woks[j]);
         }
     }
     
+    sort(subN.begin(), subN.end());
+    subN.erase(unique(subN.begin(), subN.end()), subN.end()); 
+
     for (auto x : subN) {
         if (x <= N) dp[x] = 1;
     }

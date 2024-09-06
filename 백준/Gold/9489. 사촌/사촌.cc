b27fd578be5f7b84a72645acb63af5d109bc0913
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-09-06 14:58:32 +0900

    [Gold IV] Title: 사촌, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.cc
+++ 백준/Gold/9489. 사촌/사촌.cc
@@ -11,43 +11,33 @@ int main() {
         int n, k; cin >> n >> k;
         if (n == 0 && k == 0) return 0;
 
-        vector<int> seq(n);
-        // map<int, vector<int>> ptoc;
-        // map<int, int> ctop;
-        for (int i = 0; i < n; i++) {
+        vector<int> seq(n+1);
+        for (int i = 1; i <= n; i++) {
             int x; cin >> x;
             seq[i] = x;
         }
 
-        int MAX = seq[n-1];
-        vector<vector<int>> ptoc(MAX+1);
-        vector<int> ctop(MAX+1);
+        vector<int> ctop(n+1, -1);
 
         int idx = 1;
-        for (int i = 0; i < n; i++) {
-            int root = seq[i];
-            while (idx < n) {
-                int value = seq[idx];
-                ptoc[root].push_back(value);
-                ctop[value] = root;
+        for (int i = 2; i <= n; i++) {
+            ctop[i] = idx;
 
-                if (seq[idx+1] > value+1) {
-                    idx++;
-                    break;
-                }
-                idx++;
-            }
+            if (idx <= n && seq[i+1] > seq[i]+1) idx++;
         }
 
-        int myP = ctop[k];
-        int myGP = ctop[myP];
-        int count = 0;
-        for (auto x : ptoc[myGP]) {
-            if (x == 0 || x == myP) continue;
-
-            count += ptoc[x].size();
+        int k_idx = -1;
+        for (int i = 1; i <= n; i++) {
+            if (seq[i] == k) k_idx = i; 
         }
 
-        cout << count << '\n';
+        int myP_idx = ctop[ctop[k_idx]];
+
+        int answer = 0;
+        for (auto x : ctop) {
+            if (ctop[x] == myP_idx && x != ctop[k_idx]) answer++;
+        }
+
+        cout << answer << '\n';       
     }
 }

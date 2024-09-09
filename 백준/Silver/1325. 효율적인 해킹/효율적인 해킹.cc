75e8906251ed5b64204d9ec235d89cabb2c9067d
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-09-09 14:53:19 +0900

    [Silver I] Title: 효율적인 해킹, Time: 3188 ms, Memory: 3744 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int dfs(vector<vector<int>> &graph, vector<int> &visited, int cur) {
+    visited[cur] = 1;
+    int count = 1;
+    for (auto v : graph[cur]) {
+        if (visited[v]) continue;
+
+        count += dfs(graph, visited, v);
+    }
+
+    return count;
+}
+
+int main() {
+    FASTIO;
+
+    int N, M; cin >> N >> M;
+    vector<vector<int>> graph(N+1);
+    vector<int> relation(N+1, 0);
+
+    for (int i = 0; i < M; i++) {
+        int A, B; cin >> A >> B;
+        graph[B].push_back(A);
+        relation[A]++;
+    }
+
+    int minC = *min_element(relation.begin()+1, relation.end());
+
+    int maxC = 0;
+    vector<int> L(N+1, -1);
+    for (int i = 1; i <= N; i++) {
+        // if (relation[i] != minC) continue;
+        
+        vector<int> visited(N+1, 0);
+
+        L[i] = dfs(graph, visited, i);
+        maxC = max(maxC, L[i]);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (L[i] == maxC) cout << i << ' '; 
+    }
+}

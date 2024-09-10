f1c4a366423c3f6eb95b1edca23b10c6297d7099
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-09-10 10:25:05 +0900

    [Gold IV] Title: 명제 증명, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2224. 명제 증명/명제 증명.cc
+++ 백준/Gold/2224. 명제 증명/명제 증명.cc
@@ -6,56 +6,46 @@
 #define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-int total = 0;
-
-void bfs(map<char, vector<char>> &graph, map<char, set<char>> &rel, char start) {
-    set<char> visited = {start};
-
-    queue<char> q;
-    q.push(start);
-
-    while (!q.empty()) {
-        char top = q.front();
-        q.pop();
-
-        for (auto v : graph[top]) {
-            if (visited.find(v) != visited.end()) continue;
-
-            total++;
-            visited.insert(v);
-            rel[start].insert(v);
-            q.push(v);
-        }
-    }
-}
-
 int main() {
     FASTIO;
 
     int X; cin >> X;
     cin.ignore();
-    map<char, vector<char>> graph;
-    map<char, set<char>> rel;
-    while (X--) {
-        string line; 
-        getline(cin, line);
-        char P = line[0], Q = line[5];
+    
+    int rel[52][52] = {0, };
+    for (int i = 0; i < 52; i++) rel[i][i] = 1;
 
-        if (graph.find(P) != graph.end()) graph[P].push_back(Q);
-        else {
-            graph[P] = {Q};
-            rel[P] = {};
+    while (X--) {
+        string line;
+        getline(cin, line);
+        int P = line[0] - 'a' < 0 ? line[0] - 'A' : line[0] - 'a' + 26;
+        int Q = line[5] - 'a' < 0 ? line[5] - 'A' : line[5] - 'a' + 26;
+        rel[P][Q] = 1;
+    }
+
+    for (int k = 0; k < 52; k++) {
+        for (int P = 0; P < 52; P++) {
+            for (int Q = 0; Q < 52; Q++) {
+                if (P == Q) continue;
+
+                if (rel[P][k] && rel[k][Q]) rel[P][Q] = 1;
+                
+            }
         }
     }
 
-    for (auto v : graph) {
-        bfs(graph, rel, v.first);
+    int total = 0;
+    for (int i = 0; i < 52; i++) {
+        for (int j = 0; j < 52; j++) {
+            if (i != j && rel[i][j]) total++;
+        }
     }
 
     cout << total << '\n';
-    for (auto v : rel) {
-        for (auto x : v.second) {
-            cout << v.first << " => " << x << '\n';
+    for (int i = 0; i < 52; i++) {
+        for (int j = 0; j < 52; j++) {
+            if (i != j && rel[i][j]) cout << (i < 26 ? char(i+65) : char(i-26+97)) << " => " << (j < 26 ? char(j+65) : char(j-26+97)) << '\n';
         }
     }
+
 }

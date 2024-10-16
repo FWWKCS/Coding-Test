591855c4ad328e58bc0e2896a9812dfc7352b2d2
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-10-16 09:49:06 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+
+    map<int, string> info = {
+        {1, "11\nA B C D E F G H J L M"},
+        {2, "9\nA C E F G H I L M"},
+        {3, "9\nA C E F G H I L M"},
+        {4, "9\nA B C E F G H L M"},
+        {5, "8\nA C E F G H L M"},
+        {6, "8\nA C E F G H L M"},
+        {7, "8\nA C E F G H L M"},
+        {8, "8\nA C E F G H L M"},
+        {9, "8\nA C E F G H L M"},
+        {10, "8\nA B C F G H L M"}
+    };
+
+    cout << info[N];
+}

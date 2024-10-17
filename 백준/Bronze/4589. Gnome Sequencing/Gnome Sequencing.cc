ae36337b633bd9adade4bfd2e1048df8f68b7571
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-10-17 17:52:21 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    cout << "Gnomes:\n";
+    while (N--) {
+        int a, b, c; cin >> a >> b >> c;
+        if ((a >= b && b >= c) || (a <= b && b <= c)) cout << "Ordered\n";
+        else cout << "Unordered\n";
+    }
+}

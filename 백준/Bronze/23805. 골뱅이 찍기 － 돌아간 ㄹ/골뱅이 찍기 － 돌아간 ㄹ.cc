6596fada4d75711c78870876aeef667ebd20c9c5
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-11-16 17:01:10 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
 int main() {
+    FASTIO;
     int N; cin >> N;
     for (int r = 0; r < N*5; r++) {
         for (int c = 0; c < N*5; c++) {

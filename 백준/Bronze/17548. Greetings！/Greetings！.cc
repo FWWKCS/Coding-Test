aa9bbdae10b54a971c7203b8632a6687a8996048
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-12-11 09:58:52 +0900

    [Bronze IV] Title: Greetings!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17548. Greetings！/Greetings！.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    cout << 'h';
+    for (int i = 1; i <= (s.size()-2) * 2; i++) cout << 'e';
+    cout << 'y';
+}

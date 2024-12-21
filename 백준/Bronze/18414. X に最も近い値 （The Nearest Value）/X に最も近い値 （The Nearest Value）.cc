a91eb79fcac68b409ea44b481c9f7b155bdd49ce
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2024-12-22 00:26:28 +0900

    [Bronze IV] Title: X に最も近い値 (The Nearest Value), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18414. X に最も近い値 （The Nearest Value）/X に最も近い値 （The Nearest Value）.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, L, R; cin >> X >> L >> R;
+
+    if (X <= L) cout << L;
+    else if (X >= R) cout << R;
+    else cout << X;
+}

701f08ad8d637e649b6f384acddc9229b6448e1c
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-01-21 11:43:02 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.cc
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.cc
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
@@ -10,24 +8,24 @@ int GCD(int div, int mod) {
     return GCD(mod, div % mod); 
 }
 
+int diff[100'000];
+
 int main() {
     FASTIO;
 
     int N, S; cin >> N >> S;
-    vector<int> diff;
-
-    while (N--) {
+    for (int i = 0; i < N; i++) {
         int x; cin >> x;
-        diff.push_back(abs(S-x));
+        diff[i] = abs(S-x);
     }
 
-    if (diff.size() == 1) {
+    if (N == 1) {
         cout << diff[0];
         return 0;
     }
 
     int answer = GCD(diff[0], diff[1]);
-    for (int i = 2; i < diff.size(); i++) {
+    for (int i = 2; i < N; i++) {
         answer = GCD(diff[i], answer);
     }
 

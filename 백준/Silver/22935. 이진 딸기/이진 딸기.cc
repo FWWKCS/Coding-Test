ff33bf9e28232e02fb5005948dde4eeaf98209c6
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-06 10:19:32 +0900

    [Silver V] Title: 이진 딸기, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/22935. 이진 딸기/이진 딸기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bitset>
+using namespace std;
+
+vector<string> table(15+13);
+
+int main() {
+    for (int i = 0; i < 15; i++) {
+        bitset<4> binary(i+1);
+        string b = binary.to_string();
+
+        string s = "";
+        for (char c : b) {
+            if (c - '0') s += "딸기";
+            else s += "V";
+        }
+
+        table[i] = s;
+    }
+
+
+    for (int i = 15; i < 15+13; i++) {
+        table[i] = table[28 - i];
+    }
+    
+    int T; cin >> T;
+    while (T--) {
+        int N; cin >> N;
+        cout << table[((N-1) % 28)] << '\n';
+    }
+}

d3cf7e431d9df300055eb5c59ffb244022fb963d
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-13 10:51:51 +0900

    [Silver V] Title: 알파벳과 쿼리 (Easy), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/31825. 알파벳과 쿼리 （Easy）/알파벳과 쿼리 （Easy）.cc
+++ 백준/Silver/31825. 알파벳과 쿼리 （Easy）/알파벳과 쿼리 （Easy）.cc
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<char> str = {'0'};
+string str;
 
 int mode1(int l, int r) {
     int total = 1;
-    char current = str[l];
+    char current = str[l-1];
 
-    for (int i = l+1; i <= r; i++) {
+    for (int i = l; i < r; i++) {
         if (str[i] != current) {
             total++;
             current = str[i];
@@ -19,7 +18,7 @@ int mode1(int l, int r) {
 }
 
 void mode2(int l, int r) {
-    for (int i = l; i <= r; i++) {
+    for (int i = l-1; i < r; i++) {
         if (str[i] == 'Z') str[i] = 'A';
         else str[i] = char(str[i]+1);
     }
@@ -27,8 +26,7 @@ void mode2(int l, int r) {
 
 int main() {
     int N, Q; cin >> N >> Q;
-    string s; cin >> s;
-    for (char c : s) str.push_back(c);
+    cin >> str;
     while (Q--) {
         int m, l, r; cin >> m >> l >> r;
 

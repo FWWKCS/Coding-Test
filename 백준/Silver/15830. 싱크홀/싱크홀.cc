c0131e48f8db9012788abed8020a46ecb60b8074
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-13 11:27:23 +0900

    [Silver IV] Title: 싱크홀, Time: 0 ms, Memory: 2020 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/15830. 싱크홀/싱크홀.cc
+++ 백준/Silver/15830. 싱크홀/싱크홀.cc
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+using ld = long double;
 
 int main() {
-    double V, W, D; cin >> V >> W >> D;
-
-    // 한쪽 끝에서 맞은편으로 등속운동하여 충돌하는 초기 시간
-    // W = Vt
-    double Ht = W/V;
+    ld V, W, D; cin >> V >> W >> D;
 
     int answer = 0;
+    // 한쪽 끝에서 맞은편으로 등속운동하여 충돌하는 초기 시간 W = Vk
+    ld k = W/V;
+    
     while (true) {
-        D -= 5*Ht*Ht;
+        D -= 5*k*k;
         if (D < 0) break;
 
         answer++;
-
-        // 감속
-        V *= 0.8;
-        Ht = W/V;
+        k *= 1.25;
     }
 
     cout << answer;

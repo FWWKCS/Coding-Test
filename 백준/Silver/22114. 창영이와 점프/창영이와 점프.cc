d3ee1a2c058e7d81a9e3728d2f52c03b5cc53f91
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-13 15:38:34 +0900

    [Silver II] Title: 창영이와 점프, Time: 20 ms, Memory: 3464 KB -BaekjoonHub

---
+++ 백준/Silver/22114. 창영이와 점프/창영이와 점프.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int N, K;
+
+int main() {
+    cin >> N >> K;
+    int block[N] = {0};
+    vector<vector<int>> dp(2, vector<int>(N, 1));
+
+    for (int i = 1; i < N; i++) {
+        int L; cin >> L;
+        block[i] = L;
+    } 
+
+    int answer = 0;
+    for (int i = 1; i < N; i++) {
+        if (block[i] > K) {
+            dp[0][i] = 1;
+            dp[1][i] = dp[0][i-1] + 1;
+        }
+        else {
+            dp[0][i] = dp[0][i-1] + 1;
+            dp[1][i] = dp[1][i-1] + 1;
+        }
+
+        answer = max(answer, max(dp[0][i], dp[1][i]));
+    }
+
+    cout << answer;
+}

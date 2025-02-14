ec297b82d6c53dde9e9ddb4253c212375be70ef9
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-15 04:15:49 +0900

    [Gold V] Title: 2의 멱수의 합, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.cc
+++ 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.cc
@@ -1,25 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
 #define MOD 1'000'000'000
 using namespace std;
 
-vector<vector<int>> dp;
+int dp[1'000'001];
 
 int main() {
     int N; cin >> N;
 
-    dp.resize(20, vector<int>(N+1, 1));
-    
+    dp[0] = dp[1] = 1;
+
     for (int i = 2; i <= N; i++) {
-        for (int k = 1; k <= 19; k++) {
-            int target = i-pow(2, k);
-            if (target >= 0) {
-                dp[k][i] = (dp[k-1][i] + dp[k][target]) % MOD;
-            }
-            else dp[k][i] = dp[k-1][i];
-        }
+        if (i % 2 == 0) dp[i] = (dp[i-1] + dp[i/2]) % MOD;
+        else dp[i] = dp[i-1];
     }
 
-    cout << dp[19][N];
+    cout << dp[N];
 }

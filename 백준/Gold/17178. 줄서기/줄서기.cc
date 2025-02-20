32b9b4ba8b83e66adcf469ff7dbfa7d5d66876ab
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-02-20 13:02:38 +0900

    [Gold V] Title: 줄서기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/17178. 줄서기/줄서기.cc
+++ 백준/Gold/17178. 줄서기/줄서기.cc
@@ -39,20 +39,16 @@ int main() {
 
     while (!line.empty()) {
         auto current = next.front();
-        // cout << "찾습니다: " << current.first << '-' << current.second << '\n';
 
         if (!wait.empty() && wait.top() == current) {
-            // cout << wait.top().first << '-' << wait.top().second << "  대기에서 나오세요\n\n";
             wait.pop();
             next.pop();
         }
         else if (line.front() != current) {
-            // cout << line.front().first << '-' << line.front().second << "  대기로 들어가세요\n\n";
             wait.push(line.front());
             line.pop();
         }
         else if (line.front() == current) {
-            // cout << "줄에서 나오세요\n\n";
             line.pop();
             next.pop();
         }
@@ -60,10 +56,8 @@ int main() {
 
     while (!wait.empty()) {
         auto current = next.front();
-        // cout << "찾습니다: " << current.first << '-' << current.second << '\n';
 
         if (wait.top() == current) {
-            // cout << wait.top().first << '-' << wait.top().second << "  대기에서 나오세요\n\n";
             wait.pop();
             next.pop();
         }

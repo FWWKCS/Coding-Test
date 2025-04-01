51a244d3200dd86157b1d73d15a8d0cd1bb07685
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-04-01 14:52:44 +0900

    [Bronze IV] Title: Boiling Water, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21612. Boiling Water/Boiling Water.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int B; cin >> B;
+    int P = 5*B-400;
+
+    cout << P << '\n';
+    if (P < 100) cout << 1;
+    else if (P > 100) cout << -1;
+    else cout << 0;
+}

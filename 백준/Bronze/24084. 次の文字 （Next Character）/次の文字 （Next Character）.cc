df5dab220b5c055b7c7f104bd499968611f7eecf
퍼박 <129183205+FWWKCS@users.noreply.github.com>
2025-04-27 17:38:54 +0900

    [Bronze IV] Title: 次の文字 (Next Character), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24084. 次の文字 （Next Character）/次の文字 （Next Character）.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    string S; cin >> S;
+
+    for (int i = 0; i < N-1; i++) {
+        if (S[i+1] == 'J') cout << S[i] << '\n';
+    }
+}

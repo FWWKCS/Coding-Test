b9bac05ee7b82baeefa7ad5518166d928f769017
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-11-28 11:41:20 +0900

    [Silver V] Title: 가지 산사태, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27940. 가지 산사태/가지 산사태.cc
+++ 백준/Silver/27940. 가지 산사태/가지 산사태.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
 int main() {
+    FASTIO;
+    
     int N, M, K; cin >> N >> M >> K;
     int S = 0;
     for (int p = 1; p <= M; p++) {

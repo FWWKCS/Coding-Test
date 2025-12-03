c59251994e1130a52cad52d42f55307c4ef909fc
KCS <129183205+FWWKCS@users.noreply.github.com>
2025-12-03 14:40:13 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 68 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.cc
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.cc
@@ -1,31 +1,42 @@
 #include <iostream>
 #include <vector>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
 int main() {
+    FASTIO;
+
     int N, K; cin >> N >> K;
     vector<int> A(N);
     for (auto &x : A) cin >> x;
 
-    vector<int> save = {0};
+    // vector<int> save = {0};
 
-    // int save = 0;
+    int count = 0;
+    int save = 0;
     for (int i = 1; i < N; i++) {
         int loc = i-1;
         int newItem = A[i];
 
         while (0 <= loc and newItem < A[loc]) {
             A[loc+1] = A[loc];
-            save.push_back(A[loc+1]);    
+            // save.push_back(A[loc+1]);
+            if (count < K) {
+                save = A[loc+1];    
+                count++;
+            }
             loc--;
         }
         if (loc+1 != i) {
             A[loc+1] = newItem;
-            save.push_back(A[loc+1]);
+            if (count < K) {
+                save = A[loc+1];    
+                count++;
+            }
         }
     }
 
     // for (auto x : save) cout << x << ' ';
-    if (save.size()-1 < K) cout << -1;
-    else cout << save[K];
+    if (count < K) cout << -1;
+    else cout << save;
 }
